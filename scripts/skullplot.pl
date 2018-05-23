1a5e312a89cf1d835629e04791ecc2eb5fa4a0c5
Joseph Brenner <doomvox@gmail.com>
2018-05-22 21:09:30 -0700

    moved routines to modules-- interfaces still need cleanup

--- scripts/skullplot.pl
+++ scripts/skullplot.pl
@@ -107,9 +107,14 @@ mkpath( $working_area ) unless( -d $working_area );
 
 # TODO dev only: remove when shipped.
 use FindBin qw( $Bin );
-use lib ("$Bin/../lib/", "$Bin/../../Data-BoxFormat/lib");
+use lib ("$Bin/../lib/",
+         "$Bin/../../Data-BoxFormat/lib",
+         "$Bin/../../Data-Classify/lib",
+         "$Bin/../../Graphics-Skullplot/lib");
 
 use Data::BoxFormat;
+use Data::Classify;
+use Graphics::Skullplot;
 
 my $dbox_file = shift;
 
@@ -117,6 +122,7 @@ unless( $dbox_file ) {
   die "An input data file (*.dbox) is required.";
 }
 
+# TODO maybe yagni out this *_spec shit
 if( $dependent_spec && not( $independent_spec ) ) {
   die "When using dependents option, also need independents.";
 } elsif( $independent_spec && not( $dependent_spec ) ) {
@@ -137,10 +143,16 @@ if ( $dependent_spec ) {
   $indie_count = 1;
 }
 
-my $fn = 
-  generate_filenames( $dbox_file, $working_area );
-my $dbox_name = $fn->{ base };
-my $tsv_file  = $fn->{ tsv };
+my $gsp = Graphics::Skullplot->new();
+
+# my $filenames = 
+#   generate_filenames( $dbox_file, $working_area );
+
+my $filenames = 
+ $gsp->generate_output_filenames( $dbox_file, $working_area );  # TODO fixup interface
+
+my $dbox_name = $filenames->{ base };
+my $tsv_file  = $filenames->{ tsv };
 
 ($DEBUG) && print "input dbox name: $dbox_name\nintermediate tsv_file: $tsv_file\n";
 
@@ -150,252 +162,24 @@ $dbx->output_to_tsv( $tsv_file );
 
 my @header = @{ $dbx->header() };
 
+### moving this to a new module: Data::Classify
+# my $fd = 
+#   classify_fields( $indie_count, \@header );
+my $dc_opt = { dependent_spec   => $dependent_spec,
+               independent_spec => $independent_spec,
+             };
+my $dc = Data::Classify->new;
 
-### TODO this should be a module of it's own: Data::Classify? 
-my $fd = 
-  classify_fields( $indie_count, \@header );
+my $field_metadata = 
+  $dc->classify_fields_simple( $indie_count, \@header, $dc_opt );  # TODO fixup interface
 
 ### TODO move these to Graphics::SkullPlot
-plot_tsv_to_png( $fn, $fd );
-
-my $png_file = $fn->{ png };
-exec_to_display_png( $png_file, $image_viewer );
-
-
-
-### 
-# end main, into the subs
-
-=item generate_filenames
-
-Example usage:
-
-  my $fn = 
-    generate_filenames( $dbox_file, $working_area );
-  my $dbox_name = $fn->{ base };
-  my $tsv_file  = $fn->{ tsv };
-  my $png_file  = $fn->{ png };
-
-=cut 
-
-sub generate_filenames {
-  my $dbox_file    = shift;
-  my $working_area = shift;
-
-  ### generate a tsv file in the working area, along with rscript
-  my $dbox_name = basename( $dbox_file );
-  ( my $tsv_name     = $dbox_name ) =~ s{ \.dbox $ }{.tsv}x;
-  ( my $rscript_name = $dbox_name ) =~ s{ \.dbox $ }{.r}x;
-
-  my $tsv_file     = "$working_area/$tsv_name";
-  my $rscript_file = "$working_area/$rscript_name";
-
-  ( my $png_name     = $dbox_name ) =~ s{ \.dbox $ }{.png}x;
-  my $png_file     = "$working_area/$png_name";
-
-  my %filenames =
-    (
-     base    => $dbox_name,
-     tsv     => $tsv_file,
-     rscript => $rscript_file,
-     png     => $png_file
-     );
-  return \%filenames;
-}
-
-=item classify_fields
-
-Chooses the x and y axis fields, splitting up 
-the fields in the header according the the number 
-of specified independent fields.
-
-Returns a hash of field data, indicating how
-different columns should be handled in the plotting
-stage, the keys are:
-
-  x
-  y
-  gb_cats
-  independents
-
-Example usage:
-
-   my $fd = 
-     classify_fields( $indie_count, \@header );
-
-
-=cut 
-
-sub classify_fields {
-  my $indie_count  = shift // 1;
-  my $header       = shift;
-  my $fn           = shift;
-
-  # use first col as the default independent variable (x-axis)
-  my $independent_default  = $header[ 0 ];
-
-  my ($dep_list, $indep_list);
-  my ( @dep_fields, @indep_fields, @gb_cats, $x_field );
-  if ( $indie_count == 1 ) { # special casing the most common, for code clarity
-    $x_field = $independent_default;
-    @indep_fields = $header[ -1 ]; # TODO bug no?  just the last column.
-    @gb_cats      = @header[ 1 .. ($#header-1)  ];
-  } elsif ( $indie_count > 1 ) { ### TODO this branch seems broken
-    $x_field = $independent_default;
-    my $last_idx = $#header;
-    # note above that something was broken-- restoring old form for now.
-    # @indep_fields = @header[ $last_idx - ($indie_count-1) .. $last_idx ];
-    # @gb_cats      = @header[ 1 .. ( $last_idx - $indie_count ) ];
-    @gb_cats      = @header[ 0 .. ($indie_count-1) ];
-    @indep_fields = @header[ $indie_count .. $last_idx ];
-  } elsif ( $dependent_spec || $independent_spec ) {
-    $dep_list     = $dependent_spec   || join ',', @header;
-    $indep_list   = $independent_spec || $independent_default;
-    @dep_fields   = split /[,\|]/, $dep_list;
-    @indep_fields = split /[,\|]/, $indep_list;
-    $x_field       = shift( @indep_fields );
-    @gb_cats      = @dep_fields;
-  }
-
-  my $y_field = $indep_fields[ 0 ]; ### TODO DEBUG just ploting the first to start with
-  print STDERR "x_field: $x_field\n";
-  print STDERR "y_field: $y_field\n";
-  # my $plot_code = 'qplot( skull$' . $x_axis . ', skull$' . $y_field . ' )';
-
-  my %field_data =
-    ( x           => $x_field       || '',    # instead, should have dep_fields, no?
-      y           => $y_field       || '',    # redundant with indep_fields
-      gb_cats     => [ @gb_cats ],
-      indep_field => [ @indep_fields ],
-      );
-
-  return \%field_data;
-}
-
-=item plot_tsv_to_png
-
-Generate the r-code to plot the tsv file data as the png file.
-Takes two arguments, the hash of file definitions and 
-the hash of field metadata.
-
-   x-axis  ...  y-axis  ... 
-
-   x-axis  gb-cat1  ... y-axis  ... 
-
-   x-axis  gb-cat1  gb-cat2  ... y-axis  ... 
-
-Example usages:
-
-#  plot_tsv_to_png( $x_field, $y_field, $gb_cats, $fn );
-
-  plot_tsv_to_png( $fn, $fd );
-
-=cut 
-
-sub plot_tsv_to_png {
-  my $fn = shift;
-  my $fd = shift; 
-#  my ($x_field, $y_field, $gb_cats) = @{ $fd->{ qw( x  y  gb_cats ) }}; # hash slice (mangled)
-
-  my $x_field = $fd->{ x };
-  my $y_field = $fd->{ y };
-  my $gb_cats = $fd->{ gb_cats };
-
-  my $tsv_file     = $fn->{ tsv };
-  my $rscript_file = $fn->{ rscript };
-  my $png_file     = $fn->{ png };
-  
-  my @gb_cats = @{ $gb_cats }; # TODO cleanup 
-
-  # TODO
-  # At present, limited to two group by categories (in addition to the x-axis)
-  # Maybe: if there's more than 2, fuse them together into a compound cat, hand to colour
-  my $gb_cat1 = $gb_cats[0] if $gb_cats[0];
-  my $gb_cat2 = $gb_cats[1] if $gb_cats[1];
-
-  # plot code
-  my $pc = 'ggplot( skull, ' ;
-  $pc .= '               aes(' ;
-  $pc .= "                    x = $x_field," ;
-  $pc .= "                    y = $y_field, " ;
-  $pc .= "                    colour = $gb_cat1," if $gb_cat1;
-  $pc .= "                    shape  = $gb_cat2 " if $gb_cat2;
-  $pc .= '                          ))' ;
-  $pc .= ' + geom_point( ' ;
-  $pc .= "              size  = 2.5 " ;
-  $pc .= '              )  ' ;
-
-  # Generate the file of R code to run with an Rscript call
-  # (in debug mode, make it a standalone unix script)
-  my $r_code;
-  $r_code = qq{#!/usr/bin/Rscript} . "\n" if $DEBUG;
-
-  $r_code .=<<"__END_R_CODE";
-library(ggplot2)
-skull <- read.delim("$tsv_file", header=TRUE)
-png("$png_file") # send plot output to png
-$pc
-graphics.off()   # doesn't chatter like dev.off
-__END_R_CODE
-
-  print $r_code, "\n" if $DEBUG;
-
-  open my $out_fh, '>', $rscript_file;
-  print { $out_fh } $r_code;
-  close( $out_fh );
-
-  # in case you want to run the rscript standalone
-  chmod 0755, $rscript_file;
-
-  # chdir( "$HOME/tmp" ) or die "$!";
-
-  my $erroff = '2>/dev/null';
-  $erroff = '' if $DEBUG;
-
-  my $cmd = "Rscript $rscript_file $erroff";
-
-  print STDERR "cmd:\n$cmd\n" if $DEBUG;
-  system( $cmd );
-}
-
-
-=item exec_to_display_png
-
-Open the given png file in an image viewer
-(does an exec as a final step: this should be the last
-thing called by a script).
-
-The image viewer can be set as the second, optional field.
-The default image viewer is ImageMagick's "display".
-
-Example uses:
-
-   exec_to_display_png( $png_file );
-
-   exec_to_display_png( $png_file, $image_viewer );
-
-=cut
-
-sub exec_to_display_png {
-  my $png_file     = shift;
-  my $image_viewer = shift; # effective default is 'display'
-
-  my $erroff = '2>/dev/null';
-  $erroff = '' if $DEBUG;
-
-  # Defaulting to ImageMagick's "display" is good, because I can
-  # use a dependency on Perlmagick to ensure that it's available.
-  my $vcmd;
-  if ( $image_viewer ) { 
-    $vcmd = qq{ $image_viewer $png_file $erroff };
-  } else {
-    $vcmd = qq{ display -title 'skullplot'  $png_file $erroff };
-  }
-  exec( $vcmd );
-}
-
-
-
+## plot_tsv_to_png( $filenames, $field_metadata );
+$gsp->plot_tsv_to_png( $filenames, $field_metadata );
+
+my $png_file = $filenames->{ png };
+## exec_to_display_png( $png_file, $image_viewer );
+$gsp->exec_to_display_png( $png_file, $image_viewer );
 
 #######
 ### end main, into the subs
